aaeb4c836a5a736b44badf963aca31ce3ba732b2
Vyacheslav Gidenko <mr.gidenko@gmail.com>
2015-06-03 11:14:02 -0700

    removedwhite spaces in the resumeBuilder.js file and fixed letters to be lowercase

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,10 +8,10 @@ var bio = {
 		"github": "gidenkvs",
 		"twitter": "@fakeAddress",
 		"location": "Salem, OR"
-	}, 
-    "Welcome": "Hello and Welcome to my website!",
-    "skills": ["PL/SQl Programer", "Unisys Imaging System Administrator", "OBIEE Developer and Administrator", "Beginer Web Developer"
-    ],
+	},
+	"welcome": "Hello and Welcome to my website!",
+	"skills": ["PL/SQl Programer", "Unisys Imaging System Administrator", "OBIEE Developer and Administrator", "Beginer Web Developer"
+	],
 	"bioPic": "images/main_img-small.jpg"
 };
 
@@ -50,7 +50,7 @@ bio.diplay = function() {
 
 		formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
 		
-	    $("#skills").append(formattedSkill);
+		$("#skills").append(formattedSkill);
 
 		formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
 		
@@ -69,32 +69,27 @@ bio.diplay();
 // education
 
 var education = {
-	"schools": [
-	{
+	"schools": [{
 		"name": "Chemeketa Commumity College",
 		"location": "Salem, OR",
 		"degree": "None",
 		"majors": ["Business Administrator"],
 		"dates": 2003,
 		"url": "www.chemeketa.edu"
-	},
-	{
+	}, {
 		"name": "Sacramento City College",
 		"location": "Sacramento, CA",
 		"degree": "None",
 		"majors": ["Computer Programing"],
 		"dates": 1998,
 		"url": "www.scc.losrios.edu"
-	}		
-	],
-	"onlineCourses": [
-	{
+	}],
+	"onlineCourses": [{
 		"title": "JavaScript Crash Course",
-   		"school": "Udacity",
-   		"dates": 2015,
+		"school": "Udacity",
+		"dates": 2015,
 		"url": "http://www.udacity.com"
-	}	
-	]	
+	}]	
 };
 
 education.diplay = function() {
@@ -142,29 +137,25 @@ education.diplay();
 //works experience
 
 var work = {
-	"jobs": [
-	{
+	"jobs": [{
 		"employer": "Oregon Employment Department",
 		"title": "Information Systems Specialist",
 		"dates": "June 2009 - Present",
 		"location": "Salem, OR",
 		"description": "Developing Federal Reports, data analytics, imaging system administration, and web design."
-	},
-	{
+	}, {
 		"employer": "Oregon Finish and Design",
 		"title": "Project Manager",
 		"dates": "Sept 2004 - June 2009",
 		"location": "Salem, OR",
 		"description": "Managing construction projects, products procurement, and public relations."
-	},	
-	{
+	}, {
 		"employer": "United Dominion Realty Trust",
 		"title": "Maintenance Tech",
 		"dates": "Sept 2000 - Sept 2004",
 		"location": "Salem, OR",
 		"description": "Multi-unit property maintenance."
-	}
-	]
+	}]
 };
 
 work.diplay = function() {
@@ -172,7 +163,7 @@ work.diplay = function() {
 
 		$("#workExperience").append(HTMLworkStart);
 
-	    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
 		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
 
 		var formattedEmployerTitle = formattedEmployer + formattedTitle;
@@ -194,20 +185,17 @@ work.diplay();
 // projects
 
 var projects = {
-	"projects": [
-	{
+	"projects": [{
 		"title": "Sample Project 1",
 		"dates": "2014 - 2015",
 		"description": "Automation of Federal Reports via Oracle Datawarehouse and Pl/SQL",
 		"images": ["images/rosebt.jpg"]
-	},
-	{
+	}, {
 		"title": "Sample Project 2",
 		"dates": "2015 - present",
 		"description": "Modification of look and feel of company website",
 		"images": ["images/image_UI.jpg"]
-	}
-	]
+	}]
 };
 
 projects.diplay = function() {
@@ -221,26 +209,26 @@ projects.diplay = function() {
 		$(".project-entry:last").append(formattedDates);
 
 		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-		$(".project-entry:last").append(formattedDescription);				
+		$(".project-entry:last").append(formattedDescription);
 
 		if (projects.projects[project].images.length > 0) {
 			for (image in projects.projects[project].images) {
 				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
 				$(".project-entry:last").append(formattedImage);
 			}
-		} 
+		}
 	}
 }
 
 projects.diplay();
 
 function inName(name) {
-    name = bio.name.trim().split(" ");
-    console.log(name);
-    name[1] = name[1].toUpperCase();
-    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+	name = bio.name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
 
-    return name[0] + " " + name[1];
+	return name[0] + " " + name[1];
 }
 
 $('#main').append(internationalizeButton);
